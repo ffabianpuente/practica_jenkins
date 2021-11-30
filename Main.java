68c34cc9b1098375006d01eb1e538111234b4a99
unknown <fj.fabianpuente@gmail.com>
2021-11-30 13:01:24 -0500

    solucionando el error del ;

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo desde Java") 
+		System.out.println("Hola mundo desde Java");
 		System.out.println("La conezión con github se realizó correctamente --- mensaje nuevo rama dev");
 	}
 }
